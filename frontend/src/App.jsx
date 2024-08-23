63634fdd4621d719655f22a36670a2d9b095650f
shivaraj-kc <shivarajchandaragi9@gmail.com>
2024-08-23 10:33:37 +0530

    react rotes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,24 @@
 import './App.css'
-function App() {
+import {
+  BrowserRouter, Routes , Route,
+} from 'react-router-dom'
+import Signup from './components/Signup'
+import Signin from './components/Signin'
+import SendMoney from './components/SendMoney'
+import Dashboard from './components/Dashboard'
 
+function App() {
   return (
-    <div>
-        Hello world
-    </div>
+<>
+<BrowserRouter>
+<Routes>
+  <Route path='/signup' element={<Signup/>}/>
+  <Route path='/signin' element={<Signin/>}/>
+  <Route path='/send' element={<SendMoney/>}/>
+  <Route path='/dashboard' element={<Dashboard/>}/>
+</Routes>
+</BrowserRouter>
+</>
   )
 }
 
