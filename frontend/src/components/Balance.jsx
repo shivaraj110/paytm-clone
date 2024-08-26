d5e1626cdfcaf0de5f47806f45eefbf2c76d509e
shivaraj-kc <shivarajchandaragi9@gmail.com>
2024-08-26 20:57:44 +0530

    added a custom hook for fetching balance

--- frontend/src/components/Balance.jsx
+++ frontend/src/components/Balance.jsx
@@ -1,27 +1,12 @@
-import axios from "axios"
-import { useEffect, useState } from "react"
-import { useRecoilState } from "recoil"
-import { currUserBalAtom } from "../atoms/currUserBal"
-
+import { useBal } from "../hooks/useBal"
 export const Balance = () => {
-    const token = localStorage.getItem("token")
-    const [bal,setBal] = useRecoilState(currUserBalAtom)
-    useEffect(()=>{
-            axios.get("http://localhost:3000/api/v1/account/balance",{
-            headers:{
-                Authorization : 'Bearer ' + token
-            }
-        }
-    ).then(res=>{
-        setBal(res.data.balance)
-    })
-    },[token])
+    const bal = useBal(6)
     return <div className="flex mt-20">
         <div className="font-bold text-lg pl-3">
             Your balance
         </div>
         <div className="font-semibold ml-4 text-lg">
-            Rs {bal}
+             {bal}.INR
             {}
         </div>
     </div>
